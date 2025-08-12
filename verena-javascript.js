368a20f3ce1e00497eda9207b2a74263ccafd676
Angelo Zero <angelo.akm@gmail.com>
2025-08-12 20:54:42 -0300

    aula programacao

--- verena-javascript.js
+++ verena-javascript.js
@@ -20,7 +20,25 @@ alert(nome_do_usuario3);
 // Alerta do tipo NUMERO
 //alert(4281)
 
+var numero_casa = 4281
+var valor_nascimento1 = 1990
+var valor_nascimento2 = 1993
+
+alert(numero_casa);
+alert(valor_nascimento1);
+alert(valor_nascimento2);
+
+var soma_dos_numeros = (numero_casa + valor_nascimento1 + valor_nascimento2)
+
+alert(soma_dos_numeros)
+
+
+
 
 /**** APENAS LOGICO ****/
 // Alerta do tipo LOGICO
 //alert(4281 > 1200)
+
+var e_maior = soma_dos_numeros > 5000
+alert("a soma dos numeros é maior que 5000?")
+alert(e_maior);
